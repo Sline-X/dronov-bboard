6b11434d8ce8f4e99dc504dcb1864ae87afb850e
Sline-X <kipa.maksim99@gmail.com>
2023-05-02 23:56:36 +0300

    add author posts

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,6 +5,7 @@
 @section('main')
 <h2>{{ $bb->title }}</h2>
 <p>{{ $bb->content }}</p>
-<p>{{ $bb->price }}</p>
+<p>{{ $bb->price }} руб.</p>
+<p>Автор: {{ $bb->user->name }} </p>
 <p><a href="{{ route('index') }}">На перечень объявлений</a></p>
 @endsection('main')
