dfc11287018ea92b9ecbe7242d0a9ac5912a4e5d
Sline-X <kipa.maksim99@gmail.com>
2023-12-31 00:09:22 +0300

    add migration example

--- database/migrations/2023_04_16_204005_create_bbs_table.php
+++ database/migrations/2023_04_16_204005_create_bbs_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -20,6 +21,17 @@ return new class extends Migration
                   ->onDelete('cascade');
             $table->timestamps();
             $table->index('created_at');
+//            $table->softDeletes();
+//            ->default('asd');
+//            $table->float('random')
+//                ->default(new Expression('RAND()'));
+//            $table->text('desc')->nullable();
+//            $table->unsignedTinyInteger('order')->index();
+//            $table->string('name', 40)->index('idx_name');
+//            $table->index(['name', 'order']); //индекс по нескольким полям
+//            $table->rawIndex(new Expression('upper(name)'), 'idx_name');
+//            $table->unsignedBigInteger('rubric_id');
+//            $table->foreign('rubric_id')->references('id')->on('rubrics');
         });
     }
 
